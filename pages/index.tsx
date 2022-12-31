c5d8cac2d71c07a86a66d32c5be8b2fd87b7d51e
yoshihiro-tada <63399807+yoshihiro-tada@users.noreply.github.com>
2022-12-31 22:38:31 +0900

    Profile Page CSS 調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -108,8 +108,10 @@ export default function Home() {
                 <Card sx={{ minWidth: 275 }}>
                   <CardContent>
                     <h3>誕生</h3>
-                    <time>1991/07/10</time>
-                    <p>香川県で生まれました。</p>
+                    <time className={stylesProfile.time}>1991/07/10</time>
+                    <div className={stylesProfile.historyContent}>
+                      <p>香川県で生まれました。</p>
+                    </div>
                   </CardContent>
                 </Card>
               </TimelineContent>
@@ -124,11 +126,13 @@ export default function Home() {
                 <Card sx={{ minWidth: 275 }}>
                   <CardContent>
                     <h3>高知工科大学 入学•卒業</h3>
-                    <time>2011/04 to 2015/03</time>
-                    <p>
-                      幼い頃から自然環境や生物に興味があり、環境理工学群に進学しました。
-                      生物•化学を中心に専攻し、海藻の成分からバイオ燃料を生成する研究をしていました。
-                    </p>
+                    <time className={stylesProfile.time}>2011/04 to 2015/03</time>
+                    <div className={stylesProfile.historyContent}>
+                      <p>
+                        幼い頃から自然環境や生物に興味があり、環境理工学群に進学しました。
+                        生物•化学を中心に専攻し、海藻の成分からバイオ燃料を生成する研究をしていました。
+                      </p>
+                    </div>
                   </CardContent>
                 </Card>
               </TimelineContent>
@@ -143,12 +147,16 @@ export default function Home() {
                 <Card sx={{ minWidth: 275 }}>
                   <CardContent>
                     <h3>モラブ阪神工業株式会社 入社•辞職</h3>
-                    <time>2015/04 to 2019/09</time>
-                    <p>
-                      SESの企業に入社しました。学生時代はITに触れる機会はそれほどありませんでしたが、社会人研修のExcel•VBA研修を通じてITに興味を持つようになりました。<br></br>
-                      やはりSESという業務形態なので、思うようなスキルの向上が難しく、たまたま興味を持ったLinux(Ubuntu)を使ってどんなコマンドがあるのか学習したり、ローカルのファイルサーバーを立ち上げてリモート接続を試してみたりしました。
-                      次にHTML/CSSに興味を持ち、JavaScript(JQuery)、PHP(WordPress)...というようにWebに関係した技術の学習を進めていきました。ある程度学習が進んだ段階で、地元でWebデザイナー•フロントエンジニアとして働きたいと思い辞職しました。
-                    </p>
+                    <time className={stylesProfile.time}>2015/04 to 2019/09</time>
+                    <div className={stylesProfile.historyContent}>
+                      <p>
+                        SESの企業に入社しました。学生時代はITに触れる機会はそれほどありませんでしたが、社会人研修のExcel•VBA研修を通じてITに興味を持つようになりました。<br></br>
+                      </p>
+                      <p>
+                        やはりSESという業務形態なので、思うようなスキルの向上が難しく、たまたま興味を持ったLinux(Ubuntu)を使ってどんなコマンドがあるのか学習したり、ローカルのファイルサーバーを立ち上げてリモート接続を試してみたりしました。
+                        次にHTML/CSSに興味を持ち、JavaScript(JQuery)、PHP(WordPress)...というようにWebに関係した技術の学習を進めていきました。ある程度学習が進んだ段階で、地元でWebデザイナー•フロントエンジニアとして働きたいと思い辞職しました。
+                      </p>
+                    </div>
                   </CardContent>
                 </Card>
               </TimelineContent>
@@ -163,12 +171,16 @@ export default function Home() {
                 <Card sx={{ minWidth: 275 }}>
                   <CardContent>
                     <h3>株式会社フクシン 入社</h3>
-                    <time>2020/06</time>
-                    <p>
-                      コロナ禍という背景もありながら、なんとか就職することができました。
-                      WordPressを使えるということもあり、入社当初から企業サイトのリニューアルを任せていただきました。
-                      今では企業サイトを含めた複数のサイトの更新、SNSの運用、インフルエンサーマーケティング、クラウドファンディングの管理、通販サイト(パッケージ型:futureshop)の管理などに携わっています。
-                    </p>
+                    <time className={stylesProfile.time}>2020/06</time>
+                    <div className={stylesProfile.historyContent}>
+                      <p>
+                        コロナ禍という背景もありながら、なんとか就職することができました。
+                        WordPressを使えるということもあり、入社当初から企業サイトのリニューアルを任せていただきました。
+                      </p>
+                      <p>
+                        今では企業サイトを含めた複数のサイトの更新、SNSの運用、インフルエンサーマーケティング、クラウドファンディングの管理、通販サイト(パッケージ型:futureshop)の管理などに携わっています。
+                      </p>
+                    </div>
                   </CardContent>
                 </Card>
               </TimelineContent>
@@ -183,12 +195,14 @@ export default function Home() {
                 <Card sx={{ minWidth: 275 }}>
                   <CardContent>
                     <h3>香川コーディングブートキャンプ 受講</h3>
-                    <time>2022/07 to 2022/11</time>
-                    <p>
-                      4ヶ月間のブートキャンプに参加しました。
-                      受講のきっかけは、社内業務だけだとどうしてもモダンな開発に触れる機会がなく、Webに関する理解のある社員も自分だけだったので次第にスキルの頭打ち感やもどかしさを感じたためです。
-                      受講により得たものは多かったですが、特にMVCの考え方やチーム開発の進め方などが非常に学びが大きかったです。
-                    </p>
+                    <time className={stylesProfile.time}>2022/07 to 2022/11</time>
+                    <div className={stylesProfile.historyContent}>
+                      <p>
+                        4ヶ月間のブートキャンプに参加しました。
+                        受講のきっかけは、社内業務だけだとどうしてもモダンな開発に触れる機会がなく、Webに関する理解のある社員も自分だけだったので次第にスキルの頭打ち感やもどかしさを感じたためです。
+                        受講により得たものは多かったですが、特にMVCの考え方やチーム開発の進め方などが非常に学びが大きかったです。
+                      </p>
+                    </div>
                   </CardContent>
                 </Card>
               </TimelineContent>

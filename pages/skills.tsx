df1e8ee3a71d4ff6c275a223130e794f52d7fdb9
yoshihiro-tada <63399807+yoshihiro-tada@users.noreply.github.com>
2023-01-01 22:02:36 +0900

    test2

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -117,7 +117,7 @@ export default function Home() {
                       <h2>{skill.name}</h2>
                       <Rating
                         name="read-only"
-                        defaultValue={skill.rating}
+                        defaultValue={{skill.rating}
                         readOnly />
                     </div>
                   </div>

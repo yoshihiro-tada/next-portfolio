df6436c5a4ce95ff94cb416d4693dcf5af8d5a53
yoshihiro-tada <63399807+yoshihiro-tada@users.noreply.github.com>
2023-01-15 21:43:23 +0900

    OGP修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,30 +54,6 @@ export default function Home({ history }:Prop) {
       <NextSeo
         title="Portfolio-Site | Yoshihiro Tada"
         description="こちらのポートフォリオサイトは、 Next.jsで作りVercelにデプロイして公開しています。"
-        openGraph={{
-          url: 'https://www.url.ie/a',
-          title: 'Open Graph Title',
-          description: 'Open Graph Description',
-          images: [
-            {
-              url: 'https://www.example.ie/og-image-01.jpg',
-              width: 800,
-              height: 600,
-              alt: 'Og Image Alt',
-              type: 'image/jpeg',
-            },
-            {
-              url: 'https://www.example.ie/og-image-02.jpg',
-              width: 900,
-              height: 800,
-              alt: 'Og Image Alt Second',
-              type: 'image/jpeg',
-            },
-            { url: 'https://www.example.ie/og-image-03.jpg' },
-            { url: 'https://www.example.ie/og-image-04.jpg' },
-          ],
-          siteName: 'SiteName',
-        }}
       />
       <div className={styles.layout}>
         <header className={styles.header}>
